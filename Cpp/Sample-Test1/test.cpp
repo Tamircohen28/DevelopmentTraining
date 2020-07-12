43cc96c15aa981bd84a117ab0d678793ba2a861d
Tamir Cohen <tamircohen2468@gmail.com>
2020-07-12 13:23:14 +0300

    Gardians improved

---
+++ Cpp/Sample-Test1/test.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "gtest/gtest.h"
+
+int add(int x, int y) { return x + y; }
+int multiply(int x, int y) { return x * y; }
+
+TEST(maths, AddTest) {
+  EXPECT_EQ(add(5, 4), 9);
+}
+
+TEST(maths, MulTest) {
+	EXPECT_EQ(multiply(5, 4), 20);
+}
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}

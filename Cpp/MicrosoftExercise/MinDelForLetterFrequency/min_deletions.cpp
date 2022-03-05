f838597ba3ce9ebabb41f39a009d5dd3aa2808c1
Tamir Cohen <tamircohen2468@gmail.com>
2022-03-05 18:28:31 +0200

    starting microsoft exresice, done minDeletions

---
+++ Cpp/MicrosoftExercise/MinDelForLetterFrequency/min_deletions.cpp
@@ -0,0 +1,117 @@
+/**
+    Minimum Deletions to Make Character Frequencies Unique: 
+    A string s is called good if there are no two different
+    characters in s that have the same frequency. 
+    Given a string s, returns the minimum number of characters
+    needed to delete to make s good.
+
+    Constrains:
+        * 1 <= s.length <= 10^5
+        * s contains only lowercase English letters.
+        
+    @file min_deletions.cpp
+    @author Tamir Cohen
+    @version 1.0 05/03/2022 
+*/
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+// 'a' is the first ascii char of lowercase English letters
+const unsigned short INDEX_SHIFT{'a'};
+const unsigned short NUM_OF_EN_LETTERS{26};
+
+void debug(unsigned long int arr[NUM_OF_EN_LETTERS])
+{
+    for (int i = 0; i < NUM_OF_EN_LETTERS; i++)
+    {
+        cout << "'" << (char)(i + INDEX_SHIFT) << "': " << arr[i] << endl;
+    }
+}
+
+/**
+    Return the minimum number of characters you need to delete to make s good.
+    @param s any string (under given constrains)
+    @return minimum number of characters you need to delete
+*/
+int minDeletions(string s)
+{
+    unsigned long int frequency[NUM_OF_EN_LETTERS]{0};
+
+    // looping through the characters of s, counting frequency
+    for (char &c : s)
+        frequency[c - INDEX_SHIFT] += 1;
+
+    // quick sort algorithm on const size array ~ O(1)
+    sort(frequency, frequency + NUM_OF_EN_LETTERS, greater<int>());
+
+    int deletions{0};
+
+    for (size_t i = 1; i < NUM_OF_EN_LETTERS; i++)
+    {
+        // the array is sorted in descending order, so if we at 0 we are done
+        if (frequency[i] == 0)
+        {
+            break;
+        }
+
+        // check if frequency needs to be reduced
+        if (frequency[i] >= frequency[i - 1])
+        {
+            // frequency to 0
+            if (frequency[i - 1] == 0)
+            {
+                deletions += frequency[i];
+                frequency[i] = 0;
+            }
+            // frequency to lowast - 1
+            else
+            {
+                deletions += frequency[i] - (frequency[i - 1] - 1);
+                frequency[i] = frequency[i - 1] - 1;
+            }
+        }
+    }
+    return deletions;
+}
+
+int main()
+{
+    string s = "aab";
+    int del = minDeletions(s);
+
+    if (del == 0)
+    {
+        cout << "s is already good." << endl;
+    }
+    else
+    {
+        cout << "You need to make " << del << " deletions" << endl;
+    }
+
+    s = "aaabbbcc";
+    del = minDeletions(s);
+
+    if (del == 0)
+    {
+        cout << "s is already good." << endl;
+    }
+    else
+    {
+        cout << "You need to make " << del << " deletions" << endl;
+    }
+
+    s = "ceabaacb";
+    del = minDeletions(s);
+
+    if (del == 0)
+    {
+        cout << "s is already good." << endl;
+    }
+    else
+    {
+        cout << "You need to make " << del << " deletions" << endl;
+    }
+}
--- Cpp/PhysicsVector/vectors.cpp
+++ Cpp/PhysicsVector/vectors.cpp
@@ -3,33 +3,91 @@
 --------------------------------------------------------------------*/
 #include <iostream>
 #include <String>
+#include <cmath>
 using namespace std;
 
 class Vector3D
 {
-public:
-    double x = 0, y = 0, z = 0;
+private:
+    double x = 0, y = 0, z = 0, length = 0;
 
+    void operation_debug(string msg, Vector3D vec)
+    {
+        cout << "Doing " << msg << " of myself: (" + to_str() + "), and second: (" + vec.to_str() + ")" << endl;
+    }
+
+public:
     // parameters constructor
-    Vector3D(double x, double y, double z) : x(x), y(y), z(z) {}
+    Vector3D(double x, double y, double z) : x(x), y(y), z(z)
+    {
+        length = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
 
     string to_str()
     {
         return "x: " + to_string(x) + " y: " + to_string(y) + " z: " + to_string(z);
     }
 
-    // calculate addition of second vector and return result as new Vector3D
-    Vector3D add(Vector3D second)
+    // calculate addition of self and antoher vector and return result as new Vector3D
+    Vector3D add(Vector3D vec)
     {
-        cout << "Doing Addition of myself: (" + to_str() + "), and second: (" + second.to_str() + ")" << endl;
-        return Vector3D(x + second.x, y + second.y, z + second.z);
+        operation_debug("Addition", vec);
+        Vector3D result = Vector3D(x + vec.x, y + vec.y, z + vec.z);
+        cout << "Addition result: " << result.to_str() << endl;
+        return result;
+    }
+
+    // calculate substucrtion of self and antoher vector, return result as new Vector3D
+    Vector3D sub(Vector3D vec)
+    {
+        operation_debug("Substucrtion", vec);
+        Vector3D result = Vector3D(x - vec.x, y - vec.y, z - vec.z);
+        cout << "Substucrtion result: " << result.to_str() << endl;
+        return result;
+    }
+
+    // dot_product (scalar multiplication) of self and another vector
+    double dot_product(Vector3D vec)
+    {
+        operation_debug("Dot Product", vec);
+        double result = x * vec.x + y * vec.y + z * vec.z;
+        cout << "Dot Product result: " << result << endl;
+        return result;
+    }
+
+    // cross_product (vector multiplication) of self and another vector, return result as new Vector3D
+    double cross_product(Vector3D vec)
+    {
+        operation_debug("Cross Product", vec);
+        double result = x * vec.x + y * vec.y + z * vec.z;
+        cout << "Cross Product result: " << result << endl;
+        return result;
+    }
+
+    // scalar multiplication of self and scalar, return result as new Vector3D
+    Vector3D mul(double scalar)
+    {
+        Vector3D result = Vector3D(x * scalar, y * scalar, z * scalar);
+        cout << "Scalar Mul of " << scalar << " result: " << result.to_str() << endl;
+        return result;
     }
 };
 
 int main()
 {
-    Vector3D a(1, 2, 3);
-    Vector3D b(1, 2, 3);
-    Vector3D result = a.add(b);
-    cout << "Result: " << result.to_str() << endl;
+    // Q1 given vectors
+    Vector3D u(2, -3, 1);
+    Vector3D v(1, -2, -1);
+    Vector3D w(2, 3, 0);
+
+    // section a
+    // s = 2u - v + 3w
+    Vector3D s = u.mul(2).sub(v).add(w.mul(3));
+    cout << "\n$> s: " << s.to_str() << endl
+         << endl;
+
+    // r = u + v + w
+    Vector3D r = u.add(v).add(w);
+    cout << "\n$> r: " << r.to_str() << endl
+         << endl;
 }

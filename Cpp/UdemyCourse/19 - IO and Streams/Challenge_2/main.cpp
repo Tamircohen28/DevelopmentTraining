ef856deed15ff6282c46d42e2577635b1801b64d
Tamir Cohen <tamircohen2468@gmail.com>
2022-03-08 11:11:58 +0200

    Added more udemy course sections

---
+++ Cpp/UdemyCourse/19 - IO and Streams/Challenge_2/main.cpp
@@ -0,0 +1,13 @@
+// Section 19
+// Challenge 2
+// Automated Grader
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello world" << endl;
+    return 0;
+}
---
+++ Cpp/UdemyCourse/19 - IO and Streams/Challenge_3/main.cpp
@@ -0,0 +1,13 @@
+// Section 19
+// Challenge 3
+// Word counter
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << endl;
+    return 0;
+}
